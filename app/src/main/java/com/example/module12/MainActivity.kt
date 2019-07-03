7827d39c870f52d94008686612f92c174eba5333
gurupatel-sa <gururaj.patel.sa@gmail.com>
2019-07-03 17:03:24 +0530

    Changes in MainActivity

--- app/src/main/java/com/example/module12/MainActivity.kt
+++ app/src/main/java/com/example/module12/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        FusedLocationDemo.setOnClickListener(View.OnClickListener {
+        MapsExample.setOnClickListener(View.OnClickListener {
             var intent = Intent(this , FusedLocationExample::class.java)
             startActivity(intent)
         })
