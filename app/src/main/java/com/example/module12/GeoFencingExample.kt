1e0657fd6ecb24da3d562d3c2800eee0ad8c6926
gurupatel-sa <gururaj.patel.sa@gmail.com>
2019-07-04 13:04:44 +0530

    GeoFencing Service added

--- app/src/main/java/com/example/module12/GeoFencingExample.kt
+++ app/src/main/java/com/example/module12/GeoFencingExample.kt
@@ -1,12 +1,33 @@
 package com.example.module12
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingClient
+import com.google.android.gms.location.GeofencingRequest
 
 class GeoFencingExample : AppCompatActivity() {
 
+    private var geofencingClient:GeofencingClient?=null
+    private val requestId ="Azia"
+    @SuppressLint("RestrictedApi", "MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_geo_fancing_example)
+
+        geofencingClient=GeofencingClient(this)
+
+        var geofence= Geofence.Builder().setRequestId(requestId).setCircularRegion(57.0 , -145.0 ,10f)
+            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+            .build()
+        var geofencingRequest =GeofencingRequest.Builder().addGeofence(geofence).build()
+
+        var intent = Intent(this , GeofenceTransitionsIntentService::class.java)
+        var pendingIntent :PendingIntent = PendingIntent.getService(this , 0 , intent , 0)
+
+        geofencingClient?.addGeofences(geofencingRequest ,pendingIntent)
     }
 }
--- app/src/main/java/com/example/module12/GeofenceTransitionsIntentService.kt
+++ app/src/main/java/com/example/module12/GeofenceTransitionsIntentService.kt
@@ -1,2 +1,11 @@
-package com.example.module12 
+package com.example.module12
 
+import android.app.IntentService
+import android.content.Intent
+
+class GeofenceTransitionsIntentService(var name:String) : IntentService(name) {
+    override fun onHandleIntent(intent: Intent?) {
+
+    }
+
+}
